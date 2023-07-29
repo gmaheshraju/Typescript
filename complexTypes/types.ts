27fc77c788e8f2b95422c79aa7ec76e95059727e
Mahesh Guntumadugu <g.maheshlearn@outlook.com>
2023-07-29 19:37:32 +0530

    inital changes

--- complexTypes/types.ts
+++ complexTypes/types.ts
@@ -1,8 +1,32 @@
 console.log('hello, world!');
 
-function getFirstElement(arr : number[]) {
+function getFirstElement(arr : number[]) : (string | number) {
   return arr[0];
 }
 
-let result : number = getFirstElement([1,2,3]);
+let result : string | number = getFirstElement([1,2,3]);
 console.log(result)
+
+
+function getElement<T>(arr : T[]) {
+  return arr[0];
+}
+
+let numberArr  = getElement<number>([1,2,3]);
+console.log(numberArr);
+
+
+let stringArr  = getElement<string>(["one", "two", "three"]);
+console.log(stringArr);
+
+
+function swap <T>(a: T,b : T) : [T, T]{
+  return [b, a];
+}
+
+let swapres = swap<string>("one","two");
+console.log(swapres)
+
+
+let swapres2 = swap<number>(1,2);
+console.log(swapres2)
